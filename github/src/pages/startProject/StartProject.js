cffd456790a5f3e348334a89024a070cc8a11baf
krystel <krystel.dev@gmail.com>
2022-02-16 13:12:45 +0100

    upDate startProject

--- github/src/pages/startProject/StartProject.js
+++ github/src/pages/startProject/StartProject.js
@@ -17,13 +17,22 @@ const StartProject = () => {
             Crear un repositorio nuevo
           </Link>
         </li>
-        <li>Crear una carpeta local donde guardaremos el proyecto</li>
-        <li>Abir esta carpeta con tu terminal</li>
-        <li>Nombre carpeta % "git init"</li>
-        <li>Nombre carpeta % "git remote add origin https://git..."</li>
-        <li>Nombre carpeta % "git pull origin main"</li>
-        <li>Nombre carpeta % "git branch main"</li>
-        <li>Nombre carpeta % "git checkout main"</li>
+        <li>
+          Abir esta carpeta con tu terminal, donde quieres guardar la carpeta
+          del proyeto
+        </li>
+        <li>
+          Nombre carpeta % git clone
+          https://github.com/TuUsuarioGit/NombreRepositorio.git
+        </li>
+        <p>
+          Aparecerá instantaneamente dentro de la carpeta que hayamos
+          seleccionado, una carpeta con el nombre del repositorio creado. Si has
+          seleccionado, en la fse de crear respositorio, Add README.md, verás
+          ese archivo dentro de la carpeta. Incluso si escogido Add gitignore
+          (por ejemplo tyoe: Node, para que no suba toda esa carpeta, desde VSC
+          la verás creada.)
+        </p>
         <ol>... listo para trabajar ...</ol>
       </ol>
     </Fragment>
